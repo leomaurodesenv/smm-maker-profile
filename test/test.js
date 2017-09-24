2ee8aa7e659dc18738708ebefe9a69b23db571ff
leomaurodesenv <leo.mauro.desenv@gmail.com>
2017-09-24 16:45:29 -0300

    Update SmmMakerProfile with new CoursesWrapper (getting all courses data)

--- test/test.js
+++ test/test.js
@@ -4,12 +4,11 @@ const SmmMakerProfile = require("../lib/SmmMakerProfile.js");
 // ## Try capture some data from ID Nintendo user
 // For example to get infos of BRKsEDU
 // link: https://supermariomakerbookmark.nintendo.net/profile/BRKsEDU
-
 console.time('testing');
 
 //SmmMakerProfile.getMetaContent('BRKsEDU', function(error, makerProfile) {
-SmmMakerProfile.getMetaCoursesContent('BRKsEDU', function(error, makerProfile) {
-//SmmMakerProfile.getAllContent('BRKsEDU', function(error, makerProfile) {
+//SmmMakerProfile.getMetaCoursesContent('BRKsEDU', function(error, makerProfile) {
+SmmMakerProfile.getAllContent('BRKsEDU', function(error, makerProfile) {
     if(error) console.log('> Page not found or Nintendo ID inexistent.');
     else console.log(makerProfile);
     console.timeEnd('testing');
