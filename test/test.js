16fa872f02be36a46a30732b715dcc26158ad526
leomaurodesenv <leo.mauro.desenv@gmail.com>
2019-04-20 10:26:28 -0300

    fix condition patterns issues

--- test/test.js
+++ test/test.js
@@ -8,7 +8,7 @@ console.time("test");
 //SmmMakerProfile.getMetaContent("BRKsEDU", function(error, makerProfile) {
 SmmMakerProfile.getMetaCoursesContent("BRKsEDU", function(error, makerProfile) {
 //SmmMakerProfile.getAllContent("BRKsEDU", function(error, makerProfile) {
-    if(error) console.log("> Page not found or Nintendo ID inexistent.");
-    else console.log(makerProfile);
+    if (error) { console.log("> Page not found or Nintendo ID inexistent."); }
+    else { console.log(makerProfile); }
     console.timeEnd("test");
 });
